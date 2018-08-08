57945c22581ee80b990ba1580be097f69dcde9a1
lacau <lacau@zpay.com.br>
2018-08-08 16:44:25 -0300

    Add springboot base application.

---
+++ src/main/java/com/lacoin/LacoinApplication.java
@@ -0,0 +1,12 @@
+package com.lacoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LacoinApplication.class, args);
+	}
+}
