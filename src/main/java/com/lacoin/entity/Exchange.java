4f60fede11ae84f20e8ecdfbda8ef959c8fc2f03
lacau <lacau@zpay.com.br>
2018-08-09 16:50:02 -0300

    Add lombok dependency. Add flyway init script. Create exchange jpa mapping.

---
+++ src/main/java/com/lacoin/entity/Exchange.java
@@ -0,0 +1,31 @@
+package com.lacoin.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Exchange {
+
+    @Id
+    @GeneratedValue(generator = "EXCHANGE_SEQ_GEN", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "EXCHANGE_SEQ_GEN", sequenceName = "seq_exchange")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "code", nullable = false)
+    private short code;
+}
