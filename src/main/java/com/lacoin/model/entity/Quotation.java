52c4f18ee85ffa46d00cba2e9cfbf669d25a3723
lacau <lacau@zpay.com.br>
2018-08-11 00:38:41 -0300

    Added flyway script to create quotation table. Jpa quotation mapping.

---
+++ src/main/java/com/lacoin/model/entity/Quotation.java
@@ -0,0 +1,43 @@
+package com.lacoin.model.entity;
+
+import com.lacoin.LacoinContants;
+import java.math.BigDecimal;
+import java.time.Instant;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Quotation {
+
+    @Id
+    @GeneratedValue(generator = "QUOTATION_SEQ_GEN", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "QUOTATION_SEQ_GEN", sequenceName = "seq_quotation")
+    private Long id;
+
+    @Column(name = "amount", nullable = false, precision = LacoinContants.SATOSHI_LENGTH)
+    private BigDecimal amount;
+
+    @Column(name = "volume", nullable = false, precision = LacoinContants.SATOSHI_LENGTH)
+    private BigDecimal volume;
+
+    @Column(name = "date", nullable = false)
+    private Instant date;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_exchange", nullable = false)
+    private Exchange exchange;
+}
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -2,9 +2,23 @@ CREATE TABLE exchange (
   id int8 NOT NULL,
   name VARCHAR(255) NOT NULL,
   code VARCHAR(4) NOT NULL,
-  PRIMARY KEY (id)
+  CONSTRAINT pk_exchange PRIMARY KEY (id)
 )
 WITH (OIDS=FALSE);
 
 CREATE SEQUENCE IF NOT EXISTS seq_exchange;
 ALTER SEQUENCE seq_exchange INCREMENT BY 50;
+
+CREATE TABLE quotation (
+	id int8 NOT NULL,
+	fk_exchange int8 NOT NULL,
+	amount numeric(16,8) NOT NULL,
+	volume numeric(16,8) NOT NULL,
+	date timestamp NOT NULL,
+	CONSTRAINT pk_quotation PRIMARY KEY (id),
+	CONSTRAINT fk_quotation_exchange FOREIGN KEY (fk_exchange) REFERENCES exchange(id)
+)
+WITH (OIDS=FALSE);
+
+CREATE SEQUENCE IF NOT EXISTS seq_quotation;
+ALTER SEQUENCE seq_quotation INCREMENT BY 50;
