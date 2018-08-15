b9836c829e4c18048621e0c0af3674328177835a
lacau <lacau@zpay.com.br>
2018-08-14 21:17:55 -0300

    Add quotations job logs.

--- src/main/java/com/lacoin/Schedulers.java
+++ src/main/java/com/lacoin/Schedulers.java
@@ -3,6 +3,8 @@ package com.lacoin;
 import com.lacoin.model.entity.Quotation;
 import com.lacoin.service.QuotationService;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -11,15 +13,20 @@ import org.springframework.util.CollectionUtils;
 @Component
 public class Schedulers {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Schedulers.class);
+
     @Autowired
     private QuotationService quotationService;
 
     @Scheduled(cron = "0 0/1 * * * ?")
     public void saveLastQuotation() {
+        LOGGER.info("method=saveLastQuotation, msg=Start fetch quotations job");
         final List<Quotation> quotations = quotationService.fetchQuotations();
         if (!CollectionUtils.isEmpty(quotations)) {
             quotationService.saveQuotations(quotations);
+            LOGGER.info("method=saveLastQuotation, msg=Quotations updated");
         }
+        LOGGER.info("method=saveLastQuotation, msg=Finish fetch quotations job");
     }
 
     @Scheduled(cron = "0 0 0/2 1/1 * ?")
